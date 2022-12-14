6097f7cf768d1c07e089e17aa8ad67f2bdffc3ef
Roberto J Rodriguez <robertojrodriguez21@gmail.com>
2022-12-14 00:32:05 -0600

    User accounts display on home page

--- client/src/pages/Accounts.jsx
+++ client/src/pages/Accounts.jsx
@@ -13,10 +13,56 @@ const Accounts = ({user, BASE_URL}) => {
     getUserAccounts()
   }, [])
 
+  const accountType = (type) => {
+    switch (type) {
+      case 1:
+        return 'Checking'
+        break;
+      case 2:
+        return 'Credit Card'
+        break;
+      case 3:
+        return 'Loan'
+        break;
+      default:
+        return 'N/A'
+        break;
+    }
+  }
+
   return (
-    <div className="container">
-      x
-    </div>
+    <table class="table table-hover">
+      <thead>
+        <tr>
+          <th scope="col">Name</th>
+          <th scope="col">Type</th>
+          <th scope="col">Balance</th>
+          <th scope="col">Limit</th>
+          <th scope="col">Payment Minimum</th>
+          <th scope="col">Payment Due Date</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr class="table-active">
+          {accounts.map((account) => (
+            <>
+            <th scope="row">{account.name}</th>
+            <td>{accountType(account.type)}</td>
+            <td>${account.balance.toFixed(2)}</td>
+            <td>${account.limit.toFixed(2)}</td>
+            {account.minPayment ? 
+              <>
+              <td>${account.minPayment.toFixed(2)}</td>
+              <td>{account.dueDate}</td>
+              </> : <>
+              <td>No Payment</td> 
+              <td>No Payment</td> 
+            </>}
+            </>
+          ))}
+        </tr>
+      </tbody>
+    </table>
   )
 }
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,15 +1,19 @@
+import { useNavigate } from "react-router-dom";
 import Accounts from "./Accounts";
 
 const Home = ({user, BASE_URL}) => {
+  let navigate = useNavigate()
 
   return (
     <div className="container text-start">
       <br></br>
       <h1>Welcome back {user.firstName}!</h1>
+      <hr></hr>
       <div class="container row">
-        <h3 class="col-9" >Your Accounts</h3>
-        <button type="button" class=" col-2 btn btn-success">Create Account</button>
+        <h3 class="col-10" >Your Accounts</h3>
+        <button type="button" class=" col-2 btn btn-success" onClick={() => {navigate('/createAccount')}}>Create Account</button>
       </div>
+      <br></br>
       <Accounts user={user} BASE_URL={BASE_URL} />
     </div>
   )
