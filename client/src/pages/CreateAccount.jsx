2b649f3d56e1b7749a832cf412c9622a30faac1b
Roberto J Rodriguez <robertojrodriguez21@gmail.com>
2022-12-14 16:21:19 -0600

    Updated day list in createAccount.jsx, and added/updated tips.

--- client/src/pages/CreateAccount.jsx
+++ client/src/pages/CreateAccount.jsx
@@ -56,7 +56,7 @@ const CreateAccount = ({userId, BASE_URL}) => {
         <div className="col-4 offset-4 form-floating mb-3">
           <input required onChange={handleChange} value={formValues.limit} name='limit' type="text" className="form-control" id="floatingLimit" placeholder="Account Limit"></input>
           <label htmlFor="floatingLimit">Account Limit</label>
-          <small className="text-muted">For checking and saving accounts, enter zero.</small>
+          <small className="text-muted">For checking and saving accounts, enter the number 0.</small>
         </div> : null}
         {parseInt(formValues.type) === 3 || parseInt(formValues.type) === 4 ? 
         <>
@@ -68,35 +68,36 @@ const CreateAccount = ({userId, BASE_URL}) => {
             <label htmlFor="dueDate" className="form-label mt-4">Payment Due Date</label>
             <select name='dueDate' onChange={handleChange} className="form-select" id="dueDate">
               <option>Select</option>
-              <option value={1}>1</option>
-              <option value={2}>2</option>
-              <option value={3}>3</option>
-              <option value={4}>4</option>
-              <option value={5}>5</option>
-              <option value={6}>6</option>
-              <option value={7}>7</option>
-              <option value={8}>8</option>
-              <option value={9}>9</option>
-              <option value={10}>10</option>
-              <option value={11}>11</option>
-              <option value={12}>12</option>
-              <option value={13}>13</option>
-              <option value={14}>14</option>
-              <option value={15}>15</option>
-              <option value={16}>16</option>
-              <option value={17}>17</option>
-              <option value={18}>18</option>
-              <option value={19}>19</option>
-              <option value={20}>20</option>
-              <option value={21}>21</option>
-              <option value={22}>22</option>
-              <option value={23}>23</option>
-              <option value={24}>24</option>
-              <option value={25}>25</option>
-              <option value={26}>26</option>
-              <option value={27}>27</option>
-              <option value={28}>28</option>
+              <option value={1}>1st</option>
+              <option value={2}>2nd</option>
+              <option value={3}>3rd</option>
+              <option value={4}>4th</option>
+              <option value={5}>5th</option>
+              <option value={6}>6th</option>
+              <option value={7}>7th</option>
+              <option value={8}>8th</option>
+              <option value={9}>9th</option>
+              <option value={10}>10th</option>
+              <option value={11}>11th</option>
+              <option value={12}>12th</option>
+              <option value={13}>13th</option>
+              <option value={14}>14th</option>
+              <option value={15}>15th</option>
+              <option value={16}>16th</option>
+              <option value={17}>17th</option>
+              <option value={18}>18th</option>
+              <option value={19}>19th</option>
+              <option value={20}>20th</option>
+              <option value={21}>21st</option>
+              <option value={22}>22nd</option>
+              <option value={23}>23rd</option>
+              <option value={24}>24th</option>
+              <option value={25}>25th</option>
+              <option value={26}>26th</option>
+              <option value={27}>27th</option>
+              <option value={28}>28th</option>
             </select>
+            <small className="text-muted">For the 29th, 30th, and 31st of the month select 28th.</small>
           </div>
         </> : null}
         <br></br>
