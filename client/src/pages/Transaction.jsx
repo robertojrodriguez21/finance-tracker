b68f00f453959030643b0995982cf138bae823b3
Roberto J Rodriguez <robertojrodriguez21@gmail.com>
2022-12-15 08:34:35 -0600

    Removed unnecessary imports

--- client/src/pages/Transaction.jsx
+++ client/src/pages/Transaction.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react"
 import axios from "axios"
-import { Navigate, useNavigate, useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 
 const Transaction = ({user, BASE_URL}) => {
   let { id } = useParams()
