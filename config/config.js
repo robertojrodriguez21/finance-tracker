1edf01b0a0bf8805ee673134a107de9b397b3941
Roberto J Rodriguez <robertojrodriguez21@gmail.com>
2022-12-30 13:12:20 -0600

    Removed host keys

--- config/config.js
+++ config/config.js
@@ -3,17 +3,14 @@ require('dotenv').config()
 module.exports = {
   development: {
     database: 'finance_tracker_development',
-    host: '10.0.0.1',
     dialect: 'postgres'
   },
   test: {
     database: 'finance _tracker_test',
-    host: '10.0.0.1',
     dialect: 'postgres'
   },
   production: {
     database: 'finance_tracker_production',
-    host: '10.0.0.1',
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
